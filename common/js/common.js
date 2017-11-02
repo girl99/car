5fafa4d64ba40f19a143989f491626bdbc46e800
girl99 <1105061467@qq.com>
2017-11-02 14:11:01 +0800

    first commit

---
+++ common/js/common.js
@@ -0,0 +1,90 @@
+// JavaScript Document
+var xx=document.getElementById("one1");
+xx.onmouseover = function () {
+		var yy=document.getElementById("two1");
+        yy.style.background = "#e70012";
+    };
+xx.onmouseout = function () {
+		var yy=document.getElementById("two1");
+        yy.style.background = "#002d71";
+    };
+
+var xxx=document.getElementById("one2");
+xxx.onmouseover = function () {
+		var yyy=document.getElementById("two2");
+        yyy.style.background = "#e70012";
+    };
+xxx.onmouseout = function () {
+		var yyy=document.getElementById("two2");
+        yyy.style.background = "#002d71";
+    };
+
+var xxxx=document.getElementById("one3");
+xxxx.onmouseover = function () {
+		var yyyy=document.getElementById("two3");
+        yyyy.style.background = "#e70012";
+    };
+xxxx.onmouseout = function () {
+		var yyyy=document.getElementById("two3");
+        yyyy.style.background = "#002d71";
+    };
+
+var xxxxx=document.getElementById("one4");
+xxxxx.onmouseover = function () {
+		var yyyyy=document.getElementById("two4");
+        yyyyy.style.background = "#e70012";
+    };
+xxxxx.onmouseout = function () {
+		var yyyyy=document.getElementById("two4");
+        yyyyy.style.background = "#002d71";
+    };
+
+var xxxxxx=document.getElementById("one5");
+xxxxxx.onmouseover = function () {
+		var yyyyyy=document.getElementById("two5");
+        yyyyyy.style.background = "#e70012";
+    };
+xxxxxx.onmouseout = function () {
+		var yyyyyy=document.getElementById("two5");
+        yyyyyy.style.background = "#002d71";
+    };
+
+var xxxxxxx=document.getElementById("one6");
+xxxxxxx.onmouseover = function () {
+		var yyyyyyy=document.getElementById("two6");
+        yyyyyyy.style.background = "#e70012";
+    };
+xxxxxxx.onmouseout = function () {
+		var yyyyyyy=document.getElementById("two6");
+        yyyyyyy.style.background = "#002d71";
+    };
+
+var xxxxxxxx=document.getElementById("one7");
+xxxxxxxx.onmouseover = function () {
+		var yyyyyyyy=document.getElementById("two7");
+        yyyyyyyy.style.background = "#e70012";
+    };
+xxxxxxxx.onmouseout = function () {
+		var yyyyyyyy=document.getElementById("two7");
+        yyyyyyyy.style.background = "#002d71";
+    };
+	
+var xxxxxxxxx=document.getElementById("one8");
+xxxxxxxxx.onmouseover = function () {
+		var yyyyyyyyy=document.getElementById("two8");
+        yyyyyyyyy.style.background = "#e70012";
+    };
+xxxxxxxxx.onmouseout = function () {
+		var yyyyyyyyy=document.getElementById("two8");
+        yyyyyyyyy.style.background = "#002d71";
+    };
+
+var xxxxxxxxxx=document.getElementById("one9");
+xxxxxxxxxx.onmouseover = function () {
+		var yyyyyyyyyy=document.getElementById("two9");
+        yyyyyyyyyy.style.background = "#e70012";
+    };
+xxxxxxxxxx.onmouseout = function () {
+		var yyyyyyyyyy=document.getElementById("two9");
+        yyyyyyyyyy.style.background = "#002d71";
+    };
